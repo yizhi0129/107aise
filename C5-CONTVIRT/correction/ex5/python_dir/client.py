8af2ee0260be6e57976dc9ccee8a887453964b90
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2023-12-04 16:38:36 +0100

    Correction pawn

--- C5-CONTVIRT/correction/ex5/python_dir/client.py
+++ C5-CONTVIRT/correction/ex5/python_dir/client.py
@@ -5,17 +5,17 @@ import hashlib
 
 known_pass = {}
 
+
+print("COUCOU LES AMIS")
+
 with open("pwd.txt", "r") as f:
+    print("Processing file")
     data = f.read()
     pwd = str(data).split("\n")
 
     for p in pwd:
         md5 = hashlib.md5(p.encode())
         known_pass[md5.hexdigest()] = p
-
-    print(known_pass)
-
-
 
 
 def dictmd5(md5):
@@ -26,9 +26,10 @@ def dictmd5(md5):
     return None
 
     
+print("Before redis")
+r = redis.Redis(host="leredis")
 
-r = redis.Redis(host="localhost")
-
+print("After redis")
 
 while True:
 
